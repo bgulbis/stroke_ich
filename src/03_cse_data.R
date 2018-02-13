15bb0957f4ce19cb5a3447260705d465c513e572
bgulbis <brian.gulbis@gmail.com>
2018-02-13 09:45:07 -0600

    get teresa's data

--- src/03_cse_data.R
+++ src/03_cse_data.R
@@ -13,3 +13,25 @@ mbo_fin <- concat_encounters(pts$fin)
 
 # run MBO query
 #   * Identifiers - by FIN
+
+id <- read_data(dir_raw, "id-fin", FALSE) %>%
+    as.id()
+
+mbo_id <- concat_encounters(id$millennium.id)
+
+# run MBO query
+#   * Vitals - BP
+
+vitals <- read_data(dir_raw, "vitals", FALSE) %>%
+    as.vitals() %>%
+    filter(vital %in% c("systolic blood pressure", "arterial systolic bp 1")) %>%
+    arrange(millennium.id, vital.datetime) %>%
+    group_by(millennium.id) %>%
+    mutate(sbp_160 = (vital.result < 160 & lag(vital.result) < 160),
+           sbp_140 = (vital.result < 140 & lag(vital.result) < 140)) %>%
+    left_join(id, by = "millennium.id") %>%
+    ungroup() %>%
+    select(fin, vital, vital.datetime, vital.result, sbp_160, sbp_140,
+           vital.location)
+
+write.csv(vitals, "data/external/sbp_data.csv", row.names = FALSE)
