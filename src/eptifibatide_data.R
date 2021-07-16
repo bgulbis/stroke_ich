42160f92ffa7cd91d34bae80b620beabfc4103f6
Brian Gulbis <brian.gulbis@gmail.com>
2021-07-16 11:05:51 -0500

    get data from all sheets

--- src/eptifibatide_data.R
+++ src/eptifibatide_data.R
@@ -3,8 +3,28 @@ library(readxl)
 library(lubridate)
 library(openxlsx)
 
-pts <- read_excel("U:/Data/stroke_ich/eptifibatide/raw/eptifibatide_data.xlsx", sheet = "baseline") %>%
+demog <- read_excel("U:/Data/stroke_ich/eptifibatide/raw/eptifibatide_data.xlsx", sheet = "baseline") %>%
     rename_all(str_to_lower) %>%
     distinct() %>%
-    mutate(across(fin, as.character))
+    mutate(across(c(weight, hgb, platelets, scr, egfr, nihss), as.numeric))
+
+home_meds <- read_excel("U:/Data/stroke_ich/eptifibatide/raw/eptifibatide_data.xlsx", sheet = "home_meds") %>%
+    rename_all(str_to_lower) %>%
+    distinct()
+
+locations <- read_excel("U:/Data/stroke_ich/eptifibatide/raw/eptifibatide_data.xlsx", sheet = "locations") %>%
+    rename_all(str_to_lower) %>%
+    distinct()
+
+meds <- read_excel("U:/Data/stroke_ich/eptifibatide/raw/eptifibatide_data.xlsx", sheet = "meds") %>%
+    rename_all(str_to_lower) %>%
+    distinct()
+
+labs_vitals <- read_excel("U:/Data/stroke_ich/eptifibatide/raw/eptifibatide_data.xlsx", sheet = "labs_vitals") %>%
+    rename_all(str_to_lower) %>%
+    distinct()
+
+transfusions <- read_excel("U:/Data/stroke_ich/eptifibatide/raw/eptifibatide_data.xlsx", sheet = "transfusions") %>%
+    rename_all(str_to_lower) %>%
+    distinct()
 
