6388b0b3caa29e70aef580152ae56fc75a4963cb
Brian Gulbis <brian.gulbis@gmail.com>
2021-07-19 13:52:56 -0500

    finish data set for abstract

--- src/eptifibatide_data.R
+++ src/eptifibatide_data.R
@@ -30,18 +30,21 @@ transfusions <- read_excel("U:/Data/stroke_ich/eptifibatide/raw/eptifibatide_dat
     distinct()
 
 icu_los <- locations |>
-    filter(nurse_unit == "HH 7J")
+    filter(nurse_unit == "HH 7J") |> 
+    distinct(fin, .keep_all = TRUE)  |> 
+    select(fin, icu_los = unit_los)
 
-# how to handle multiple ICU admissions
-
-icu_num <- count(icu_los, fin)
+# icu_num <- count(icu_los, fin)
 
 target_home_meds <- c(
     "apixaban",
     "aspirin",
     "atorvastatin",
     "clopidogrel",
+    "dabigatran",
+    "edoxaban",
     "lovastatin",
+    "prasugrel",
     "pravastatin",
     "rivaroxaban",
     "rosuvastatin",
@@ -53,7 +56,7 @@ data_home_meds <- home_meds |>
     filter(medication %in% target_home_meds) |>
     distinct(fin, medication) |>
     mutate(dose = TRUE) |>
-    pivot_wider(names_from = medication, values_from = dose)
+    pivot_wider(names_from = medication, values_from = dose, names_prefix = "home_")
 
 weights <- select(demog, fin, weight)
 
@@ -94,18 +97,19 @@ eptif_bolus <- meds |>
     )
 
 eptif_bolus_first <- eptif_bolus |>
-    select(fin, start_datetime = dose_datetime) |>
-    arrange(fin, start_datetime) |>
+    select(fin, bolus_datetime = dose_datetime, bolus, bolus_units) |>
+    arrange(fin, bolus_datetime) |>
     distinct(fin, .keep_all = TRUE)
 
 eptif_drip_first <- eptif_drip |>
-    select(fin, start_datetime) |>
+    select(fin, start_datetime, stop_datetime, duration, first_rate, last_rate, max_rate, time_wt_avg_rate) |>
     distinct(fin, .keep_all = TRUE)
 
-eptif_start <- eptif_bolus_first |>
-    bind_rows(eptif_drip_first) |>
-    arrange(fin, start_datetime) |>
-    distinct(fin, .keep_all = TRUE)
+eptif_start <- eptif_drip_first |>
+    full_join(eptif_bolus_first, by = "fin") |> 
+    group_by(fin) |> 
+    mutate(begin_datetime = min(bolus_datetime, start_datetime, na.rm = TRUE)) |> 
+    select(fin, begin_datetime, starts_with("bolus"), everything())
 
 # x <- anti_join(meds, eptif_start, by = "fin")
 
@@ -119,11 +123,73 @@ sbp_eptif <- labs_vitals |>
     distinct(fin, .keep_all = TRUE) |> 
     select(fin, sbp_before_eptif = result_val)
 
-oral_antiplt <- meds |> 
-    filter(medication %in% c("aspirin", "clopidogrel", "ticagrelor")) |> 
-    arrange(fin, dose_datetime) |> 
-    distinct(fin, medication, .keep_all = TRUE)
+oral_agents <- c(
+    "aspirin", 
+    "clopidogrel", 
+    "ticagrelor", 
+    "prasugrel",
+    "warfarin",
+    "apixaban",
+    "dabigatran",
+    "edoxaban",
+    "rivaroxaban"
+)
 
-# multiple courses of eptif
-# hgb drop only after eptif?
-# transfusion only after eptif?
+oral_antithrmb <- meds |> 
+    filter(medication %in% oral_agents) |> 
+    arrange(fin, dose_datetime) |> 
+    distinct(fin, medication, .keep_all = TRUE) |> 
+    select(fin, dose_datetime, medication) |> 
+    pivot_wider(names_from = medication, values_from = dose_datetime)
+
+tx_anticoag <- meds |> 
+    filter(
+        (medication == "heparin" & !is.na(iv_event)) | 
+            (medication == "enoxaparin" & admin_dosage > 40)
+    ) |> 
+    distinct(fin, medication, .keep_all = TRUE) |> 
+    select(fin, dose_datetime, medication) |> 
+    pivot_wider(names_from = medication, values_from = dose_datetime)
+
+hgb_drop <- labs_vitals |> 
+    inner_join(eptif_start[c("fin", "begin_datetime")], by = "fin") |> 
+    filter(
+        event == "Hgb",
+        event_datetime >= begin_datetime,
+        event_datetime <= begin_datetime + days(3)
+    ) |> 
+    mutate(across(result_val, as.numeric)) |> 
+    lab_change(id = fin, lab_datetime = event_datetime, change.by = -2, FUN = max, back = 2) |> 
+    distinct(fin, .keep_all = TRUE) |> 
+    select(fin, hgb_drop = change)
+
+blood <- transfusions |> 
+    inner_join(eptif_start[c("fin", "begin_datetime")], by = "fin") |> 
+    mutate(
+        prod = case_when(
+            str_detect(product, "RBC") ~ "prbc",
+            str_detect(product, "Plasma") ~ "ffp",
+            str_detect(product, "PHP") ~ "platelet"
+        )
+    ) |> 
+    filter(
+        prod %in% c("prbc", "ffp"),
+        event_datetime >= begin_datetime,
+        event_datetime <= begin_datetime + days(3)
+    ) |> 
+    add_count(fin, prod, name = "num_units") |> 
+    distinct(fin, prod, .keep_all = TRUE) |> 
+    select(fin, transfuse_datetime = event_datetime, blood_product = prod, num_units)
+
+data_eptif <- demog |> 
+    left_join(icu_los, by = "fin") |> 
+    left_join(data_home_meds, by = "fin") |> 
+    inner_join(eptif_start, by = "fin") |> 
+    left_join(sbp_eptif, by = "fin") |> 
+    left_join(oral_antithrmb, by = "fin") |> 
+    left_join(tx_anticoag, by = "fin") |> 
+    left_join(hgb_drop, by = "fin") |> 
+    left_join(blood, by = "fin")
+
+write.xlsx(data_eptif, "U:/Data/stroke_ich/eptifibatide/final/eptifibatide_abstract_data.xlsx")    
+
