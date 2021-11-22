3f5ff9bd3885d0acfd57bcd1c14fc6c2361ad78a
Brian Gulbis <brian.gulbis@gmail.com>
2021-11-22 15:14:03 -0600

    pull data by encntr id

--- qry/ich_sarah/ich_events.sql
+++ qry/ich_sarah/ich_events.sql
@@ -2,40 +2,15 @@ WITH PATIENTS AS (
 	SELECT DISTINCT
 		ENCOUNTER.ENCNTR_ID,
 		ENCOUNTER.PERSON_ID,
-		ENCOUNTER.REG_DT_TM
+		ENCNTR_ALIAS.ALIAS AS FIN
 	FROM
-		DIAGNOSIS,
-		ENCOUNTER,
-		NOMENCLATURE,
-		PERSON
+		ENCNTR_ALIAS,
+		ENCOUNTER
 	WHERE
-		ENCOUNTER.ORGANIZATION_ID = 1 -- Memorial Hermann Hospital
-		AND ENCOUNTER.REG_DT_TM BETWEEN 
-			pi_to_gmt(
-				TO_DATE(
-					@Prompt('Enter begin date', 'D', , mono, free, persistent, {'07/01/2017 00:00:00'}, User:0), 
-					pi_get_dm_info_char_gen('Date Format Mask|FT','PI EXP|Systems Configuration|Date Format Mask')
-				), 
-				pi_time_zone(1, @Variable('BOUSER'))
-			)
-			AND pi_to_gmt(
-				TO_DATE(
-					@Prompt('Enter end date', 'D', , mono, free, persistent, {'08/01/2021 00:00:00'}, User:1), 
-					pi_get_dm_info_char_gen('Date Format Mask|FT','PI EXP|Systems Configuration|Date Format Mask')
-				) - 1/86400, 
-				pi_time_zone(1, @Variable('BOUSER'))
-			)
-		AND ENCOUNTER.LOC_FACILITY_CD = 3310 -- HH HERMANN
-		-- AND ENCOUNTER.ADMIT_SRC_CD = 9061 -- Emergency Room
-		AND ENCOUNTER.ENCNTR_ID = DIAGNOSIS.ENCNTR_ID
-		AND DIAGNOSIS.DIAG_TYPE_CD = 26244 -- Final
-		AND DIAGNOSIS.DIAG_PRIORITY = 1
-		AND DIAGNOSIS.NOMENCLATURE_ID = NOMENCLATURE.NOMENCLATURE_ID
-		AND REGEXP_INSTR(NOMENCLATURE.SOURCE_IDENTIFIER, '^I61') > 0
-		AND NOMENCLATURE.SOURCE_VOCABULARY_CD = 641836527 -- ICD-10-CM
-		AND NOMENCLATURE.PRINCIPLE_TYPE_CD = 751 -- Disease or Syndrome
-		AND ENCOUNTER.PERSON_ID = PERSON.PERSON_ID
-		AND TRUNC((TRUNC(pi_from_gmt(ENCOUNTER.REG_DT_TM, 'America/Chicago')) - TRUNC(pi_from_gmt(PERSON.BIRTH_DT_TM, 'America/Chicago'))) / 365.25, 0) >= 18
+	    ENCNTR_ALIAS.ALIAS IN @prompt('Enter value(s) for Alias','A',,Multi,Free,Persistent,,User:0)
+		AND ENCNTR_ALIAS.ENCNTR_ALIAS_TYPE_CD = 619 -- FIN NBR
+		AND ENCNTR_ALIAS.ACTIVE_IND = 1
+		AND ENCNTR_ALIAS.ENCNTR_ID = ENCOUNTER.ENCNTR_ID
 ), CODES AS (
 	SELECT DISTINCT
 		PATIENTS.ENCNTR_ID,
