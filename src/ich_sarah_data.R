5423659b697af8e55dd0361975faea6e518c5311
Brian Gulbis <brian.gulbis@gmail.com>
2021-11-22 15:13:43 -0600

    apply inclusion / exclusion criteria

---
+++ src/ich_sarah_data.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+library(readxl)
+library(mbohelpr)
+
+# f <- "/Volumes/brgulbis/Data/stroke_ich/ich_sarah/"
+f <- "data/ich_sarah/"
+
+raw_pts <- read_excel(paste0(f, "raw/patient_list.xlsx")) |> 
+    rename_all(str_to_lower)
+
+df_include <- raw_pts |> 
+    filter(
+        (is.na(pregnant) | pregnant != "Positive"),
+        ((str_detect(admit_src, regex("tfr|transfer", ignore_case = TRUE)) & !is.na(tfr_facility)) |
+             !str_detect(admit_src, regex("tfr|transfer", ignore_case = TRUE))),
+        (!(los < 2 & str_detect(disch_disposition, "Donor|Deceased"))),
+        min_sbp > 100
+    )
+
+mbo_encntr <- concat_encounters(df_include$encntr_id, 950)
+print(mbo_encntr)
