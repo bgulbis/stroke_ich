1a3636fcfd12f877672f2a04a000303876d2e08d
Brian Gulbis <brian.gulbis@gmail.com>
2024-12-02 14:26:30 -0600

    additional data points

--- isc_jessica/qry/meds.sql
+++ isc_jessica/qry/meds.sql
@@ -44,47 +44,141 @@ WHERE
 	AND CLINICAL_EVENT.EVENT_CD IN (
 		37556077, -- alteplase
 		37558157, -- tenecteplase
-		1823215551, -- sacubitril-valsartan
+		37556007, -- acebutolol
+		37556076, -- alprostadil
+		37556093, -- AMILoride
+		37556094, -- AMILoride-hydrochlorothiazide
+		37556113, -- amLODipine
 		37556114, -- amLODipine-benazepril
+		37556200, -- atenolol
+		37556202, -- atenolol-chlorthalidone
 		37556264, -- benazepril
 		37556265, -- benazepril-hydrochlorothiazide
+		37556266, -- bendroflumethiazide
+		37556267, -- bendroflumethiazide-rauwolfia serpentina
+		37556284, -- bepridil
+		37556292, -- betaxolol
+		37556311, -- bisoprolol-hydrochlorothiazide
+		37556347, -- bumetanide
 		37556407, -- candesartan
 		37556408, -- candesartan-hydrochlorothiazide
 		37556413, -- captopril
 		37556415, -- captopril-hydrochlorothiazide
+		37556437, -- carvedilol
+		37556505, -- chlorothiazide
+		37556507, -- chlorothiazide-methyldopa
+		37556524, -- chlorthalidone
+		37556525, -- chlorthalidone-clonidine
+		37556577, -- clonidine
+		37556693, -- deserpidine-methyclothiazide
+		37556758, -- diltiazem
+		37556801, -- doxazosin
 		37556838, -- enalapril
 		37556840, -- enalapril-felodipine
 		37556841, -- enalapril-hydrochlorothiazide
+		37556866, -- eprosartan
+		37556889, -- esmolol
+		37556905, -- ethacrynic acid
+		37556949, -- felodipine
 		37557031, -- fosinopril
 		37557032, -- fosinopril-hydrochlorothiazide
+		37557039, -- furosemide
+		37557128, -- guanabenz
+		37557129, -- guanfacine
+		37557172, -- hydrALAZINE
+		37557177, -- hydrochlorothiazide
 		37557179, -- hydrochlorothiazide-irbesartan
 		37557180, -- hydrochlorothiazide-lisinopril
 		37557181, -- hydrochlorothiazide-losartan
+		37557182, -- hydrochlorothiazide-methyldopa
+		37557183, -- hydrochlorothiazide-propranolol
 		37557184, -- hydrochlorothiazide-quinapril
+		37557185, -- hydrochlorothiazide-reserpine
+		37557186, -- hydrochlorothiazide-spironolactone
 		37557187, -- hydrochlorothiazide-telmisartan
+		37557188, -- hydrochlorothiazide-timolol
+		37557189, -- hydrochlorothiazide-triamterene
 		37557190, -- hydrochlorothiazide-valsartan
+		37557202, -- hydroflumethiazide-reserpine
+		37557234, -- indapamide
 		37557327, -- irbesartan
+		37557348, -- isradipine
+		37557376, -- labetalol
 		37557440, -- lisinopril
 		37557456, -- losartan
+		37557492, -- mannitol
+		37557554, -- methyclothiazide
+		37557558, -- methyldopa
+		37557570, -- metolazone
+		37557572, -- metoprolol
+		37557598, -- minoxidil
 		37557610, -- moexipril
+		37557641, -- nadolol
+		37557675, -- niCARdipine
+		37557678, -- NIFEdipine
+		37557681, -- nimodipine
+		37557683, -- nisoldipine
+		37557688, -- nitroglycerin
+		37557689, -- nitroprusside
 		37557717, -- olmesartan
+		37557780, -- penbutolol
 		37557796, -- perindopril
+		37557841, -- pindolol
+		37557859, -- polythiazide-reserpine
+		37557895, -- prazosin
+		37557927, -- propranolol
 		37557954, -- quinapril
 		37557965, -- ramipril
+		37557977, -- reserpine
+		37558102, -- sotalol
+		37558105, -- spironolactone
 		37558154, -- telmisartan
+		37558159, -- terazosin
+		37558209, -- timolol
+		37558233, -- torsemide
 		37558240, -- trandolapril
 		37558241, -- trandolapril-verapamil
+		37558253, -- triamterene
+		37558257, -- trichlormethiazide
 		37558314, -- valsartan
+		37558329, -- verapamil
+		48069137, -- bisoprolol
+		56250547, -- eplerenone
+		111416804, -- hydralazine-isosorbide dinitrate
+		117038454, -- amLODipine-atorvastatin
+		117038523, -- bendroflumethiazide-nadolol
+		117038602, -- carteolol
 		117038715, -- eprosartan-hydrochlorothiazide
+		117038766, -- hydralazine-isosorbide dinitrate
+		117038767, -- hydrochlorothiazide-metoprolol
 		117038768, -- hydrochlorothiazide-moexipril
 		117038770, -- hydrochlorothiazide-olmesartan
+		117038850, -- mecamylamine
+		117038886, -- nesiritide
+		117038907, -- pamabrom
+		117038947, -- polythiazide
+		117038948, -- polythiazide-prazosin
+		117038969, -- rauwolfia serpentina
+		247281036, -- nebivolol
 		259555334, -- amlodipine-olmesartan
 		259555338, -- amlodipine-valsartan
+		359088148, -- clevidipine
 		390605017, -- amlodipine/hydrochlorothiazide/valsartan
+		467366049, -- aliskiren-hydrochlorothiazide
+		467366054, -- aliskiren-valsartan
 		467366065, -- amlodipine-telmisartan
+		538590863, -- aliskiren-amlodipine
 		608248871, -- azilsartan
+		608249443, -- aliskiren/amlodipine/hydrochlorothiazide
 		750915783, -- azilsartan-chlorthalidone
-		1793096556 -- amLODIPine-perindopril		
+		1049066672, -- riociguat
+		1793096556, -- amLODIPine-perindopril
+		1823215551, -- sacubitril-valsartan
+		2664288743, -- nebivolol-valsartan
+		2841974887, -- lofexidine
+		3431887709, -- amlodipine-celecoxib
+		3630724761, -- levamlodipine
+		3674349325 -- vericiguat
 	)
 	AND CLINICAL_EVENT.VALID_UNTIL_DT_TM > DATE '2099-12-31'
 	AND CLINICAL_EVENT.EVENT_ID = CE_MED_RESULT.EVENT_ID
--- isc_jessica/src/isc_jessica_data.R
+++ isc_jessica/src/isc_jessica_data.R
@@ -47,13 +47,34 @@ df_diagnosis <- raw_diagnosis |>
             str_detect(icd_10_code, "N18.6") ~ "esrd",
             str_detect(icd_10_code, "I25") ~ "cad",
             str_detect(icd_10_code, "I24.9|I20.0|I21|I22") ~ "acs"
-            
         )
     ) |> 
     filter(!is.na(pmh)) |> 
     distinct(encntr_id, pmh) |> 
     mutate(value = TRUE) |> 
     pivot_wider(names_from = pmh, values_from = value, names_sort = TRUE)
+
+df_complications <- raw_diagnosis |> 
+    mutate(
+        complication = case_when(
+            str_detect(icd_10_code, "N39") ~ "uti",
+            str_detect(icd_10_code, "I69.1|I61.8|I61.9|G97.32") ~ "hemorhg_transform",
+            str_detect(icd_10_code, "G31.9") ~ "neuro_worsen",
+            str_detect(icd_10_code, "J69") ~ "asp_pna",
+            str_detect(icd_10_code, "I82.4") ~ "dvt",
+            str_detect(icd_10_code, "I26") ~ "pe",
+            str_detect(icd_10_code, "I63|I61") ~ "stroke",
+            str_detect(icd_10_code, "R58") ~ "hemorhg_complication",
+            str_detect(icd_10_code, "I61") ~ "symptom_ich",
+            str_detect(icd_10_code, "I61.8|I61.9") ~ "nontraum_ich",
+            str_detect(icd_10_code, "T78.3") ~ "angioedema",
+            str_detect(icd_10_code, "R99") ~ "death"
+        )
+    ) |> 
+    filter(!is.na(complication)) |> 
+    distinct(encntr_id, complication) |> 
+    mutate(value = TRUE) |> 
+    pivot_wider(names_from = complication, values_from = value, names_sort = TRUE, names_prefix = "complication_")
 
 df_procs <- raw_procs |> 
     distinct(encntr_id, icd_10_pcs) |> 
@@ -75,7 +96,24 @@ df_labs_admit <- raw_labs_vitals |>
     select(encntr_id, event_abbrev, result_val) |> 
     pivot_wider(names_from = event_abbrev, values_from = result_val, names_sort = TRUE, names_prefix = "admit_")
 
-df_sbp_low <- raw_labs_vitals |> 
+# df_sbp_admit <- raw_labs_vitals |> 
+#     filter(
+#         event %in% c("systolic blood pressure", "arterial systolic bp 1"),
+#         !str_detect(result_val, "[A-Za-z]")
+#     ) |> 
+#     distinct(encntr_id, .keep_all = TRUE) |> 
+#     select(encntr_id, sbp_admit = result_val)
+
+df_sbp_24h <- raw_labs_vitals |> 
+    inner_join(raw_demog[c("encntr_id", "admit_datetime")], by = "encntr_id") |> 
+    filter(
+        event %in% c("systolic blood pressure", "arterial systolic bp 1"),
+        !str_detect(result_val, "[A-Za-z]"),
+        event_datetime <= admit_datetime + hours(24)
+    ) |> 
+    arrange(encntr_id, event_datetime)
+
+df_sbp_lt90 <- raw_labs_vitals |> 
     filter(
         event %in% c("systolic blood pressure", "arterial systolic bp 1"),
         !str_detect(result_val, "[A-Za-z]")
@@ -84,7 +122,9 @@ df_sbp_low <- raw_labs_vitals |>
         across(result_val, \(x) str_remove_all(x, "<|>")),
         across(result_val, as.numeric)
     ) |> 
-    filter(result_val < 90) |> 
+    filter(result_val < 90) 
+    
+df_sbp_low <- df_sbp_lt90 |> 
     summarize(sbp_low_datetime = min(event_datetime), .by = encntr_id)
 
 df_egfr_low <- raw_labs_vitals |> 
@@ -170,16 +210,66 @@ df_bp_meds <- raw_meds |>
     select(encntr_id, med_grp, hosp_day, duration) |> 
     pivot_wider(names_from = med_grp, values_from = c(hosp_day, duration), names_glue = "{med_grp}_{.value}")
 
+df_bp_agents <- raw_meds |> 
+    filter(
+        !medication %in% c("alteplase", "tenecteplase", "nitroglycerin"),
+        is.na(iv_event)
+    ) |> 
+    med_runtime() |> 
+    summarize(
+        across(dose_start, min),
+        across(dose_stop, max),
+        across(c(duration, num_doses), sum),
+        .by = c(encntr_id, medication, course_count)
+    ) |> 
+    summarize(
+        # across(dose_start, min),
+        # across(c(duration, num_doses), sum),
+        across(num_doses, sum),
+        .by = c(encntr_id, medication)
+    ) |> 
+    pivot_wider(names_from = medication, values_from = num_doses, names_sort = TRUE)
+
+df_bp_drips <- raw_meds |> 
+    filter(
+        !medication %in% c("alteplase", "tenecteplase"),
+        !is.na(iv_event)
+    ) |> 
+    drip_runtime() |> 
+    filter(!is.na(rate)) |> 
+    summarize_drips() |> 
+    summarize(
+        across(duration, sum),
+        .by = c(encntr_id, medication)
+    ) |> 
+    pivot_wider(names_from = medication, values_from = duration, names_sort = TRUE, names_prefix = "drip_")
+
 data_patients <- raw_demog |> 
     left_join(df_diagnosis, by = "encntr_id") |> 
     left_join(df_procs, by = "encntr_id") |> 
     left_join(df_labs_admit, by = "encntr_id") |> 
+    # left_join(df_sbp_admit, by = "encntr_id") |> 
     left_join(df_sbp_low, by = "encntr_id") |> 
     left_join(df_egfr_low, by = "encntr_id") |> 
     left_join(df_dialysis, by = "encntr_id") |> 
     left_join(df_home_meds, by = "encntr_id") |> 
     left_join(df_bp_meds, by = "encntr_id") |> 
+    left_join(df_bp_agents, by = "encntr_id") |> 
+    left_join(df_bp_drips, by = "encntr_id") |> 
     left_join(df_tpa, by = "encntr_id") |> 
+    left_join(df_complications, by = "encntr_id") |> 
     select(-encntr_id)
 
-write.xlsx(data_patients, paste0(f, "final/jessica_data.xlsx"), overwrite = TRUE)
+data_sbp_lt90 <- df_sbp_lt90 |> 
+    select(-encntr_id, -event_id)
+
+data_sbp_24h <- df_sbp_24h |> 
+    select(-encntr_id, -event_id)
+
+l <- list(
+    "patients" = data_patients,
+    "sbp_lt90" = data_sbp_lt90,
+    "sbp_first_24h" = data_sbp_24h
+)
+
+write.xlsx(l, paste0(f, "final/jessica_data.xlsx"), overwrite = TRUE)
